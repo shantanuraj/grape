ddbbd6b23dae9b5d6c9cce6f6a9ed65a671b2092
ebonstar <18689574+ebonstar@users.noreply.github.com>
2023-01-26 00:13:13 +0000

    scrape game8 wiki

--- src/wiki.ts
+++ src/wiki.ts
@@ -1,25 +1,10 @@
 import fetch from "node-fetch";
 
-import { EntityType } from "@/types";
+const getURL = (id: number) =>
+  `https://game8.co/games/Monster-Hunter-Rise/archives/${id}`;
 
-const getURL = (entity: EntityType, name: string) => {
-  const title = (() => {
-    switch (entity) {
-      case "monster":
-        return `Monster/${name}`;
-      case "armor":
-        return `Armor/${name}`;
-      default:
-        return name;
-    }
-  })();
-  const url = new URL(`https://gamecat.fun/en/index.php`);
-  url.searchParams.set("title", title);
-  return url;
-};
-
-export const getPage = (entity: EntityType, name: string) =>
-  fetch(getURL(entity, name).toString())
+export const getPage = (id: number) =>
+  fetch(getURL(id))
     .then((res) => res.text())
     .catch((err) => {
       console.error(err);
